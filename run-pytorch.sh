18cee26069539be277e6418556b88830d0ebf4a7
nafis-git <43341728+nafis-git@users.noreply.github.com>
2020-04-16 11:51:44 +0300

    Update run-pytorch.sh

--- run-pytorch.sh
+++ run-pytorch.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --partition=gpu
+#SBATCH --partition=gputest
 #SBATCH --gres=gpu:v100:1,nvme:100
-#SBATCH --time=1:00:00
+#SBATCH --time=00:15:00
 #SBATCH --mem=64G
 #SBATCH --account=project_2000859
 
@@ -12,7 +12,7 @@ module purge
 module load pytorch/1.3.0
 module list
 
-export DATADIR=/scratch/project_2000859/extract
+export DATADIR=/scratch/project_2000859/extracted
 export TMPDIR=$LOCAL_SCRATCH
 
 set -xv
