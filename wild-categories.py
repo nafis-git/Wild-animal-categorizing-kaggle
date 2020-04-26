111afb1271b36c7c26afc27dac0a0912ba163ff1
nafis-git <43341728+nafis-git@users.noreply.github.com>
2020-04-26 11:10:45 +0300

    Update wild-categories.py

--- wild-categories.py
+++ wild-categories.py
@@ -1,24 +1,14 @@
 
-# coding: utf-8
-
-# Dogs-vs-cats classification with CNNs
-#
-# In this script, we'll train a convolutional neural network (CNN,
-# ConvNet) to classify images of dogs from images of cats using
-# PyTorch. This script is largely based on the blog post [Building
-# powerful image classification models using very little
-# data](https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html)
-# by François Chollet.
-#
-# **Note that using a GPU with this script is highly recommended.**
-
 import os
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+
 import glob
 
-import cv2
+
+
 import torch
 from tqdm import tqdm_notebook
 from PIL import Image, ImageFile
@@ -26,7 +16,6 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import models
 from sklearn.model_selection import train_test_split
 from torchvision import transforms
-from skimage import io
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
@@ -48,4 +37,65 @@ df_train = pd.DataFrame({'id': [item['id'] for item in train_data['annotations']
                                 'image_id': [item['image_id'] for item in train_data['annotations']],
                                 'file_name': [item['file_name'] for item in train_data['images']]})
 
-df_train.head()
+print("df_train", df_train.head())
+
+
+df_image = pd.DataFrame.from_records(train_data['images'])
+
+indices = []
+for _id in df_image[df_image['location'] == 537]['id'].values:
+    indices.append( df_train[ df_train['image_id'] == _id ].index )
+
+for the_index in indices:
+    df_train = df_train.drop(df_train.index[the_index])
+
+
+indices = []
+for i in df_train['file_name']:
+    try:
+        Image.open('/scratch/project_2000859/mohamman/train/' + i)
+    except:
+        print(i)
+        df_train.drop(df_train.loc[df_train['file_name']==i].index, inplace=True)
+
+
+with open(r'/scratch/project_2000859/mohamman/metadata/iwildcam2020_test_information.json') as f:
+    test_data = json.load(f)
+
+df_test = pd.DataFrame.from_records(test_data['images'])
+print("df_test",df_test.head())
+
+
+batch_size = 64
+IMG_SIZE = 64
+
+N_EPOCHS = 1
+
+ID_COLNAME = 'file_name'
+ANSWER_COLNAME = 'category_id'
+TRAIN_IMGS_DIR = r'../scratch/project_2000859/mohamman/train/'
+TEST_IMGS_DIR = r'../scratch/project_2000859/mohamman/test/'
+
+
+train_df, test_df = train_test_split(df_train[[ID_COLNAME, ANSWER_COLNAME]],
+                                     test_size = 0.15,
+                                     shuffle = True
+                                    )
+
+
+print("train_df.head",train_df.head(5))
+
+
+CLASSES_TO_USE = df_train['category_id'].unique()
+print("CLASSES_TO_USE",CLASSES_TO_USE)
+
+
+CLASSMAP = dict(
+    [(i, j) for i, j
+     in zip(CLASSES_TO_USE, range(NUM_CLASSES))
+    ]
+)
+print ("CLASSMAP", CLASSMAP)
+
+
+REVERSE_CLASSMAP = dict([(v, k) for k, v in CLASSMAP.items()])
